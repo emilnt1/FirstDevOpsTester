96683183f20519fded549950b29c88b51812e0fb
Emil Trangeled <emilbfh@hotmail.com>
2022-10-04 09:08:43 +0200

    Hej ehj

---
+++ src/main/java/service/HelloService.java
@@ -0,0 +1,14 @@
+package service;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+
+@Path("hello")
+public class HelloService {
+    @GET
+    public String getHello(){
+        return "Hello Devops-API!";
+    }
+}
+
---
+++ web/src/stores/GiraffeStore.js
@@ -0,0 +1,21 @@
+import {makeAutoObservable, runInAction} from "mobx";
+
+const baseUrl = process.env.NODE_ENV === 'development' ?  "http://localhost:8080/":""; //Check if dev environment
+
+class GiraffeStore {
+    giraffes = ["Loading giraffes"];
+
+    constructor(props) {
+        makeAutoObservable(this,{},{autoBind:true});
+        this.fetchGiraffes();
+    }
+
+    fetchGiraffes (){
+        fetch(baseUrl + "api/giraffes").then(
+            (response)=> response.json().then(
+                (json)=> runInAction(()=>this.giraffes=json)
+            )
+        )
+    }
+}
+export const giraffeStore = new GiraffeStore();
